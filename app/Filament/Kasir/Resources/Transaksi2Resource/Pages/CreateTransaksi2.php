cb83a5f073cb42a29ce5814f6674a849b53bcaeb
Daalleee <dalemasan10@gmail.com>
2025-10-01 15:00:16 +0700

    Add CreateTransaksi2 page for managing return transactions

---
+++ app/Filament/Kasir/Resources/Transaksi2Resource/Pages/CreateTransaksi2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Kasir\Resources\Transaksi2Resource\Pages;
+
+use App\Filament\Kasir\Resources\Transaksi2Resource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTransaksi2 extends CreateRecord
+{
+    protected static string $resource = Transaksi2Resource::class;
+}
