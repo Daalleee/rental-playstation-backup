7e6ecac22ee960baa8cf741b976b06e957eb10f7
Daalleee <dalemasan10@gmail.com>
2025-10-01 15:01:54 +0700

    Add StatusResource for managing product statuses

---
+++ app/Filament/Pemilik/Resources/StatusResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Pemilik\Resources;
+
+use App\Filament\Pemilik\Resources\StatusResource\Pages;
+use App\Filament\Pemilik\Resources\StatusResource\RelationManagers;
+use App\Models\Status;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StatusResource extends Resource
+{
+    protected static ?string $model = Status::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Status Produk';
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama_status')
+                    ->required()
+                    ->maxLength(20),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama_status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStatuses::route('/'),
+            'create' => Pages\CreateStatus::route('/create'),
+            'view' => Pages\ViewStatus::route('/{record}'),
+            'edit' => Pages\EditStatus::route('/{record}/edit'),
+        ];
+    }
+}
