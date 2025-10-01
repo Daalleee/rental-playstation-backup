bc71fd75f7ad69635bd90f091eaaa76ac45dcd97
Daalleee <dalemasan10@gmail.com>
2025-10-01 15:01:43 +0700

    Add LaporanResource for managing reports

---
+++ app/Filament/Pemilik/Resources/LaporanResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Pemilik\Resources;
+
+use App\Filament\Pemilik\Resources\LaporanResource\Pages;
+use App\Filament\Pemilik\Resources\LaporanResource\RelationManagers;
+use App\Models\Laporan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LaporanResource extends Resource
+{
+    protected static ?string $model = Laporan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Laporan';
+    protected static ?int $navigationSort = 2;
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLaporans::route('/'),
+            'create' => Pages\CreateLaporan::route('/create'),
+            'view' => Pages\ViewLaporan::route('/{record}'),
+            'edit' => Pages\EditLaporan::route('/{record}/edit'),
+        ];
+    }
+}
